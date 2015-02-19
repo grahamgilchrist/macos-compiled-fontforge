102dc5f8e8086671de976c7671556556b46bd4fe
Graham Gilchrist <graham@incuna.com>
2015-02-19 17:18:10 +0000

    Add compiled ttfautohint and fontforge Cellar files

---
+++ fontforge/20120731/include/fontforge/config.h
@@ -0,0 +1,68 @@
+/* inc/config.h.  Generated from config.h.in by configure.  */
+#ifndef _FF_CONFIG_H
+# define _FF_CONFIG_H	1
+
+#define HAVE_DLFCN_H 1
+/* #undef _NO_PYTHON */
+#define HAVE_ICONV_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_LANGINFO_H 1
+#define HAVE_PTHREAD_H 1
+#define HAVE_LIBINTL_H 1
+/* #undef HAVE_IEEEFP_H */
+/* #undef FREETYPE_HAS_DEBUGGER */
+#define HAVE_LONG_DOUBLE 1
+#define _HAS_LONGLONG 1
+#define HAVE_LIBINTL_H 1
+#define _STATIC_LIBINTL 1
+/* #undef _STATIC_LIBUNGIF */
+/* #undef _STATIC_LIBPNG */
+/* #undef _STATIC_LIBPNG12 */
+#define _STATIC_LIBTIFF 1
+#define _STATIC_LIBJPEG 1
+/* #undef _STATIC_LIBSPIRO */
+/* #undef _STATIC_LIBXML2 */
+/* #undef _STATIC_LIBFREETYPE */
+/* #undef _STATIC_LIBUNINAMESLIST */
+/* #undef _STATIC_LIBCAIRO */
+/* #undef _NO_LIBUNINAMESLIST */
+/* #undef SHAREDIR */
+/* #undef LIBDIR */
+/* #undef PREFIX */
+#define _CursorsMustBe16x16 1
+#define _Keyboard 1
+#define __Mac 1
+/* #undef _BrokenBitmapImages */
+/* #undef _ModKeysAutoRepeat */
+/* #undef __CygWin */
+/* #undef _NO_XINPUT */
+#define FONTFORGE_CONFIG_CAPSLOCK_FOR_ALT 1
+/* #undef FONTFORGE_CONFIG_SHOW_RAW_POINTS */
+/* #undef FONTFORGE_CONFIG_TYPE3 */
+#define FONTFORGE_CONFIG_DEVICETABLES 1
+#define FONTFORGE_CONFIG_USE_DOUBLE 1
+/* #undef FONTFORGE_CONFIG_USE_LONGDOUBLE */
+/* #undef FONTFORGE_CONFIG_PASTEAFTER */
+/* #undef FONTFORGE_CONFIG_GB12345 */
+/* #undef FONTFORGE_CONFIG_TILEPATH */
+/* #undef _NO_FFSCRIPT */
+/* #undef _NO_TZSET */
+/* #undef _NO_FREETYPE */
+/* #undef _NO_LIBXML */
+#define _NO_LIBUNGIF 1
+/* #undef _NO_LIBPNG */
+/* #undef _LIBPNG12 */
+/* #undef _NO_LIBJPEG */
+/* #undef _NO_LIBTIFF */
+#define _NO_LIBSPIRO 1
+/* #undef NODYNAMIC */
+/* #undef FREETYPE_HAS_DEBUGGER */
+/* #undef _NO_SNPRINTF */
+/* #undef _NO_XKB */
+#define X_DISPLAY_MISSING 1
+#define PYTHON_LIB_NAME "python2.7"
+#define _NO_LIBCAIRO 1
+#define _NO_LIBPANGO 1
+/* #undef WORDS_BIGENDIAN */
+
+#endif
